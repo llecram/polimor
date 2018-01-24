217d4aa59d50c87cf1198e4bdf994c41468f83f1
Dennis <31170537+llecram@users.noreply.github.com>
2018-01-24 10:22:13 -0500

    Add files via upload

---
+++ polimorfismo2/employee3.h
@@ -0,0 +1,15 @@
+#ifndef EMPLOYEE3_H_INCLUDED
+#define EMPLOYEE3_H_INCLUDED
+#include<string>
+#include"employee12.h"
+class BasePlusComissionEmployee:public ComissionEmployee{
+public:
+    BasePlusComissionEmployee(const string&,const string&,const string&,double=0.0,double=0.0,double=0.0);
+    void setBaseSalary(double);
+    double getBaseSalary()const;
+    double earnings()const;
+    void print()const;
+private:
+    double baseSalary;
+};
+#endif // EMPLOYEE3_H_INCLUDED
